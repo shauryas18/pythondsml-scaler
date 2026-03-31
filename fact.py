6ec87b1d73851df832da8abc403c418a04cbec36
Shaurya Singh <shauryasingh96317@gmail.com>
2026-03-31 22:29:38 +0530

    Add files via upload

---
+++ fact.py
@@ -0,0 +1,9 @@
+# Program to calculate factorial using for loop
+
+n = int(input("Enter a number: "))
+factorial = 1
+
+for i in range(1, n + 1):
+    factorial *= i  
+
+print("Factorial of", n, "is", factorial)
