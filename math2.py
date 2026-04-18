54ed1345367d6cd6c88e488cce4281e8d9bf2ec1
Shaurya Singh <shauryasingh96317@gmail.com>
2026-04-18 23:40:52 +0530

    Add files via upload

---
+++ math2.py
@@ -0,0 +1,29 @@
+#Square Root of a Number
+import math
+
+num = float(input("Enter a number: "))
+result = math.sqrt(num)
+print("Square root is:", result)
+
+
+# Find Factorial
+import math
+num = int(input("Enter a number: "))
+result = math.factorial(num)
+print("Factorial is:", result)
+
+
+
+# Find GCD of Two Numbers
+import math
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("GCD is:", math.gcd(a, b))
+
+
+#Value of Pi and Power
+import math
+print("Value of pi:", math.pi)
+num = float(input("Enter number: "))
+power = float(input("Enter power: "))
+print("Result:", math.pow(num, power))
